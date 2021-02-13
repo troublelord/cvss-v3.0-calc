be4cdc739c1a1a4b1b2ab885787c3303ab8dfc45
troublelord <abhishekcs123@yahoo.com>
2021-02-13 19:31:13 +0530

    weight mod

--- cvss.py
+++ cvss.py
@@ -3,7 +3,7 @@ import argparse
 import sys
 
 
-cvssWeight={'AV':{'N':0.85,'A':0.62,'L':0.55,'P':0.2},'AC':{'H':0.44,'L':0.77},'PR':{'U':{'N':0.85,'L':0.62,'H':0.27},'C':{'N':0.85,'L':0.68,'H':0.5}},'UI':{'N':0.85,'R':0.62},'S':{'U':6.42,'C':7.52},'C':{'N':0,'L':0.22,'H':0.56},'I':{'N':0,'L':0.22,'H':0.56},'A':{'N':0,'L':0.22,'H':0.56}}
+cvssWeight={'AV':{'N':0.85,'A':0.62,'L':0.55,'P':0.2},'AC':{'H':0.44,'L':0.77},'PR':{'U':{'N':0.85,'L':0.62,'H':0.27},'C':{'N':0.85,'L':0.68,'H':0.5}},'UI':{'N':0.85,'R':0.62},'S':{'U':6.42,'C':7.52},'CIA':{'N':0,'L':0.22,'H':0.56}}
 
 def parse(cvss):
 	cvssDict = {}
@@ -18,7 +18,7 @@ def cvss_calc(cvss):
 	if(cvss[0].split(':')[1]=='3.0'):
 		cvssDict = parse(cvss[1:])
 		
-		iscBase = 1 - ( ( 1 - cvssWeight['C'][cvssDict['C']] ) * ( 1 - cvssWeight['I'][cvssDict['I']] ) *  ( 1 - cvssWeight['A'][cvssDict['A']] ) )
+		iscBase = 1 - ( ( 1 - cvssWeight['CIA'][cvssDict['C']] ) * ( 1 - cvssWeight['CIA'][cvssDict['I']] ) *  ( 1 - cvssWeight['CIA'][cvssDict['A']] ) )
 
 		if(cvssDict['S']=='U'):
 			exp_SubScore = 8.22 * cvssWeight['AV'][cvssDict['AV']] * cvssWeight['AC'][cvssDict['AC']] * cvssWeight['PR']['U'][cvssDict['PR']] * cvssWeight['UI'][cvssDict['UI']]
